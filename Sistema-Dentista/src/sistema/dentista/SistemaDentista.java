b72a16c4694ec50c83f0632373c170cfb7f3924a
Guga <gugatascheri@gmail.com>
2024-04-14 17:49:15 -0300

    .

---
+++ Sistema-Dentista/src/sistema/dentista/SistemaDentista.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema.dentista;
+
+/**
+ *
+ * @author tasca
+ */
+public class SistemaDentista {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
